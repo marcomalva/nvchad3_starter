7689c414bf7291b68b631499ac6a159cb4ae4eab
Marco Malva <60412224+marcomalva@users.noreply.github.com>
2026-07-17 08:24:32 -0700

    feat(display): enable absolute and relative line numbers

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.opt.number = true         -- Shows the absolute line number for your current line
+vim.opt.relativenumber = true -- Shows relative line numbers for all other lines
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
