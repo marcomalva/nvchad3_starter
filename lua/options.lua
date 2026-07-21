bdd1501643059212d52d8917a01f51c2f26c21a5
Marco Malva <60412224+marcomalva@users.noreply.github.com>
2026-07-21 06:58:36 -0700

    feat(config): enable spell check by default

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,8 @@ require "nvchad.options"
 -- add yours here!
 vim.opt.number = true         -- Shows the absolute line number for your current line
 vim.opt.relativenumber = true -- Shows relative line numbers for all other lines
+vim.opt.spell = true          -- Enable spell check
+vim.opt.spelllang = "en_us"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
